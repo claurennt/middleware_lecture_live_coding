2024895dcadd780c81f6670fc44bfda337ef2370
claurennt <c.benedetti1989@gmail.com>
2022-05-17 16:47:59 +0200

    add requestTimeLogger and getOnlyMiddleware

--- index.js
+++ index.js
@@ -4,13 +4,46 @@ require("dotenv").config();
 
 const pool = require("./db/client");
 const app = express();
+const path = require("path");
 
 const port = 3000;
+
+//serve images from public/images folder when path starts with /images
+app.use("/images", express.static(path.join(__dirname, "public/images")));
 
 // ### App-level BUILT-IN middleware to parse the body of incoming requests
 app.use(express.urlencoded({ extended: true })); // body parser middleware for html post forms, extended true allows body to accept any data types, not only strings
 
 app.use(express.json()); // body parser middleware for post requests except hmtl forms post requests
+
+const getOnlyMiddleware = (request, response, next) => {
+  if (request.method === "GET") {
+    next(); //goes to next middleware/function in the line if the request is a GET request
+  } else {
+    return response
+      .status(405)
+      .send(
+        `${request.method} request not allowed on path "${request.originalUrl}".`
+      );
+    //return response.sendStatus(403); //returns a 403 forbidden status code if the request is not a GET request
+  }
+};
+app.use("/only-get", getOnlyMiddleware);
+
+app.get("/only-get", (req, res) =>
+  res.status(200).send("Fantastic you have used the correct method!")
+);
+
+const requestTimeLogger = (request, response, next) => {
+  console.log(
+    `${request.method} ${
+      request.originalUrl
+    }: ${new Date().toLocaleTimeString()}`
+  );
+  next(); // continue with the next middleware
+};
+
+app.use("/movies", requestTimeLogger);
 
 app.get("/movies", async (req, res) => {
   try {
@@ -66,13 +99,6 @@ app.put("/movies/:id", async (req, res) => {
     res.status(500).send("Error retrieving movies from DB.");
   }
 });
-//check admin credentials
-const checkAdminToken = (req, res, next) => {
-  const { token } = req.body;
-  if (!token || token !== process.env.ADMIN_TOKEN)
-    return res.status(401).send("Wrong credentials.");
-  next();
-};
 
 app.delete("/movies", async (req, res) => {
   //destructure the key and the value from the req.body to use them in the SQL statement dynamically
